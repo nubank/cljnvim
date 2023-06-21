af51956d1e1c371bb6a037d730be66cdfe9c0b84
Zoey Pessanha <zoey.pessanha@nubank.com.br>
2023-06-21 18:18:20 -0300

    add treesitter plugins

---
+++ modules/treesitter.nix
@@ -0,0 +1,59 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  inherit (pkgs) neovimPlugins;
+  inherit (pkgs.vimPlugins) nvim-treesitter;
+  inherit (lib) mkEnableOption mkIf mkOption types;
+  cfg = config.lvim.treesitter;
+  mapPluginGrammars = grammars: nvim-treesitter.withPlugins (p: map (g: p.${g}) grammars);
+in {
+  options.lvim.treesitter = {
+    grammars = mkOption {
+      description = "Grammars packages";
+      type = types.listOf types.str;
+      default = [];
+    };
+    enable = mkEnableOption "Enables tree-sitter [nvim-treesitter]";
+  };
+
+  config.lvim = mkIf cfg.enable {
+    startPlugins = with neovimPlugins; [
+      nvim-ts-autotag
+      nvim-ts-context
+      nvim-ts-rainbow
+      (mapPluginGrammars cfg.grammars)
+    ];
+    globals = {
+      "foldmethod" = "expr";
+      "foldexpr" = "nvim_treesitter#foldexpr()";
+      "nofoldenable" = 1;
+    };
+    rawConfig = ''
+      -- TRESSITTER
+      require('nvim-treesitter.configs').setup({
+        highlight = {
+          enable = true,
+          use_languagetree = true,
+        },
+        rainbow = {
+          enable = true,
+          query = 'rainbow-parens',
+          strategy = require('ts-rainbow').strategy.global,
+        },
+        autotag = {
+          enable = true,
+        },
+      })
+
+        require'treesitter-context'.setup {
+          enable = true,
+          throttle = true,
+          max_lines = 0
+        }
+      -- END TRESSITTER
+    '';
+  };
+}
